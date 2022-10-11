26be3c869a7a65a978f35ef658e8729c519b8995
MythiaOfc <114059170+MythiaOfc@users.noreply.github.com>
2022-10-11 13:29:33 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,13 @@
+pkg update && pkg upgrade
+pkg install git -y
+pkg install nodejs -y
+pkg install ffmpeg -y
+pkg install imagemagick -y
+pkh install unzip -y
+pkg install wget -y
+rm -rf node_modules
+rm -rf session.data.json
+wget https://www.dropbox.com/s/brxofxkatcvv1gg/node_modules.zip
+unzip node_modules.zip
+
+echo "Semuanya sudah di install , ketik \"npm start\" untuk menjalankan hanaa bot"
