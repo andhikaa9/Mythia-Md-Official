a74a0e884174d5343d3de2227e7950262add811b
MythiaOfc <mail.mythiamd@gmail.com>
2022-10-11 07:33:43 +0700

    new

---
+++ plugins/info-paystore.js
@@ -0,0 +1,76 @@
+let handler = async (m, { conn }) => {
+	//-----PRICE
+//sewa
+let sh = '5'
+let sn = '15'
+let ss = '20'
+let sp = '35'
+let sv = '50'
+//premium
+let ph = '10'
+let pn = '20'
+let pp = '40'
+let pv = '50'
+let ppm = '70'
+let info = `
+Hai kak mau sewa bot, untuk grup kamu?
+Nah bisa banged ni kak kami juga menyediakan sewa bot yg murah….
+
+Sewa bot:
+💌 perbulan 5ribu
+💌 permenen 10ribu 2grub
+💌 permanen 3 grub atau lebih 20ribu
+
+TERIMAKASIH…
+
+*⫹⫺ PAYMENT:*
+• *Qris:* Minta Owner Bot!
+• *Dana:* ${pdana}
+• *Gopay:* ${pgopay}
+
+–––––– *🐾 Kebijakan* ––––––
+🗣️: Kak, Kok harganya mahal banget?
+💬: Mau tawar menawar? boleh, silahkan chat owner aja
+
+🗣️: Scam ga nih kak?
+💬: Enggalah, Owner 100% Tepati janji #STAYHALAL
+
+`
+const sections = [
+   {
+	title: `${htjava} SEWA ✦-------`,
+	rows: [
+	    {title: "🔖 𝗛𝗘𝗠𝗔𝗧", rowId: '.order *Paket:* HEMAT • Sewa', description: '𝗣𝗿𝗶𝗰𝗲: ' + sh + 'k (1 bulan)' },
+	    {title: "🔖 𝗡𝗢𝗥𝗠𝗔𝗟", rowId: '.order *Paket:* NORMAL • Sewa', description: '𝗣𝗿𝗶𝗰𝗲: ' + sn + 'k (permanen 2 grub)' },
+	{title: "🔖 𝗦𝗧𝗔𝗡𝗗𝗔𝗥", rowId: '.order *Paket:* STANDAR • Sewa', description: '𝗣𝗿𝗶𝗰𝗲: ' + ss + 'k (permanen 3 grup - 6grup)' },
+	{title: "🔖 𝗣𝗥𝗢", rowId: '.order *Paket:* PRO • Sewa', description: '𝗣𝗿𝗶𝗰𝗲: ' + sp + 'k (permanen 7 grup - 10 grup)' },
+	{title: "🔖 𝗩𝗜𝗣", rowId: '.order *Paket:* VIP • Sewa', description: '𝗣𝗿𝗶𝗰𝗲: ' + sv + 'k (permanen 10 grup - 13grup)' },
+	]
+    }, {
+    title: `${htjava} PREMIUM ✦-------`,
+	rows: [
+	    {title: "🌟 𝗛𝗘𝗠𝗔𝗧", rowId: '.order *Paket:* HEMAT • Premium', description: '𝗣𝗿𝗶𝗰𝗲: ' + ph + 'k (1 minggu)' },
+	    {title: "🌟 𝗡𝗢𝗥𝗠𝗔𝗟", rowId: '.order *Paket:* NORMAL • Premium', description: '𝗣𝗿𝗶𝗰𝗲: ' + pn + 'k (1 bulan)' },
+	{title: "🌟 𝗣𝗥𝗢", rowId: '.order *Paket:* PRO • Premium', description: '𝗣𝗿𝗶𝗰𝗲: ' + pp + 'k (4 bulan)' },
+	{title: "🌟 𝗩𝗜𝗣", rowId: '.order *Paket:* VIP • Premium', description: '𝗣𝗿𝗶𝗰𝗲: ' + pv + 'k (8 bulan)' },
+	{title: "🌟 𝗣𝗘𝗥𝗠𝗔𝗡𝗘𝗡𝗧", rowId: '.order *Paket:* PERMANENT • Premium', description: '𝗣𝗿𝗶𝗰𝗲: ' + ppm + 'k (12 bulan)' },
+	]
+    },
+]
+
+const listMessage = {
+  text: info,
+  footer: botdate,
+  title: wm,
+  buttonText: "Click Here!",
+  sections
+}
+await conn.sendMessage(m.chat, listMessage)
+//conn.sendHydrated(m.chat, info, wm, null, sgc, "🌎 Group Official", null,null, [['Owner','.owner']], m)
+}
+
+handler.help = ['sewa', 'premium']
+handler.tags = ['main']
+handler.command = /^(sewa(bot)?|premium)$/i
+
+export default handler
---
+++ plugins/info-script.js
@@ -0,0 +1,31 @@
+import fetch from 'node-fetch'
+let handler = async(m, { conn, text, usedPrefix, command }) => {
+let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
+
+let str = `*CIEEE LAGI NYARI SC YAKKK:V*
+
+Nih Kakk :
+https://github.com/MythiaOfc/Mythia-Md-Official
+
+My Rest Api :
+https://api-mythiamd.herokuapp.com
+
+Sc Hanaa Full Enc Mau No Enc Recode Sendiri`
+let wibu = `https://telegra.ph/file/3238445b0b83558653806.jpg` 
+let thumb = await(await fetch(wibu)).buffer()
+conn.sendButtonDoc(m.chat, str, wm,'Thankyou','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
+    mediaUrl: "https://Instagram.com/-",
+    mediaType: "VIDEO",
+    description: "https://www.instagram.com/p/CevoCg5hG-p/?utm_source=ig_web_copy_link", 
+    title: '⫹⫺HanaaCanss Multi Device',
+    body: wm,
+    thumbnail: thumb,
+    sourceUrl: sgc
+  }
+  } }) 
+          }
+handler.help = ['source code']
+handler.tags = ['info']
+handler.command =  /^(script|sc)$/i
+
+export default handler
