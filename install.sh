57eaf0be005c5f72c38fb3ffa794ad85a99d47c9
MythiaOfc <114059170+MythiaOfc@users.noreply.github.com>
2022-10-12 13:52:58 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,5 +9,6 @@ rm -rf node_modules
 rm -rf session.data.json
 wget https://www.dropbox.com/s/brxofxkatcvv1gg/node_modules.zip
 unzip node_modules.zip
+rm -rf node_modules.zip
 
 echo "Semuanya sudah di install , ketik \"npm start\" untuk menjalankan hanaa bot"
